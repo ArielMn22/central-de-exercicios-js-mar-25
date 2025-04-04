da9b0cfb858fbcd321dbd600911212e66e63b212
ArielMn22 <arielpaixao10@gmail.com>
2025-04-03 21:53:08 -0300

    Add conditional logic and variable examples in new script file; update index to include new script

---
+++ condicionais.js
@@ -0,0 +1,19 @@
+let numero = prompt("Digite um número:"); // 0
+
+if (numero > 0) {
+
+    console.log("Positivo.");
+
+} else {
+
+    if (numero < 0) {
+
+        console.log("Negativo.");
+
+    } else {
+
+        console.log("É zero.")
+
+    }
+
+}
--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-alert("Hello World na tela!");
+// alert("Hello World na tela!");
 
-console.log("Hello World no console!");
+// console.log("Hello World no console!");
+
+let nome = "Ariel";
+const idade = 22;
+var cidade = "São Paulo";
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+console.log("Meu nome é: " + nome); // Concatenação
+console.log(`Meu nome é: ${nome}`); // Interpolação
+
+console.log("Tenho " + idade + " anos");
+console.log(`Tenho ${idade} anos`);
+
+console.log("Moro em: " + cidade);
+console.log(`Moro em: ${cidade}`);
+
+let texto = "Tenho 2 celulares"; // String
+let numero = 20; // Number
+
+console.log(texto);
+console.log(numero);
+
+console.log(typeof idade);
+
+let n1 = "10";
+let n2 = 10;
+
+let soma = Number(n1) + n2;
+
+console.log(soma);
