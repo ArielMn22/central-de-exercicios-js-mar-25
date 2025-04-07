60919462fbb983991e4fc1a65290d81d6f4442ff
ArielMn22 <arielpaixao10@gmail.com>
2025-04-07 20:34:11 -0300

    Rename intermediario01 to intermediario01comif and refactor to improve clarity; add intermediario01comswitch function for switch-case operations

--- condicionais.js
+++ condicionais.js
@@ -14,7 +14,7 @@ function exercicio01 () {
 
 }
 
-function intermediario01 () {
+function intermediario01comif () {
 
     let n1 = Number(prompt("Digite um número: "));
     let n2 = Number(prompt("Digite outro número: "));
@@ -23,22 +23,48 @@ function intermediario01 () {
 
     let resultado;
 
+    // IF
     if (operacao == "+") {
-
         resultado = n1 + n2;
-
     } else if (operacao == "-") {
-
         resultado = n1 - n2;
-
     } else if (operacao == "*") {
-
         resultado = n1 * n2;
-
     } else if (operacao == "/") {
-
         resultado = n1 / n2;
+    }
 
+    alert(resultado);
+
+}
+
+function intermediario01comswitch () {
+
+    let n1 = Number(prompt("Digite um número: "));
+    let n2 = Number(prompt("Digite outro número: "));
+
+    let operacao = prompt("Digite a operação: ");
+
+    let resultado;
+
+    // SWITCH
+    switch (operacao) {
+        case "+":
+            resultado = n1 + n2;
+            break;
+        case "-":
+            resultado = n1 - n2;
+            break;
+        case "*":
+            resultado = n1 * n2;
+            break;
+        case "/":
+            resultado = n1 / n2;
+            break;
+    
+        default:
+            alert("Operação inválido. Saia daqui agora!")
+            break;
     }
 
     alert(resultado);
